4167e536647f2e593434f4396d9549c8aee69464
Evgeniy.Lychkovsky <cdsa1995@mail.ru>
2023-12-21 16:49:00 +0300

    Added new code

--- src/Kyu6/DeadFish/Kata.java
+++ src/Kyu6/DeadFish/Kata.java
@@ -5,32 +5,37 @@ import java.util.Arrays;
 public class Kata {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(parse("iiiso")));
+        System.out.println(Arrays.toString(parse("iiisdoso")));
     }
 
     public static int[] parse(String data) {
 
-        int value = 0;
+        double value = 0;
         int[] endpoint = new int[0];
         int[] secondpoint = new int[endpoint.length + 1];
+        int[] endvalue = new int[secondpoint.length + 1];
+        String testzxc = null;
 
         char[] test = data.toCharArray();
 
         for (int j = 0; j < test.length; j++) {
-            if(test[j] == 'i') {
+            if (test[j] == 'i') {
                 value++;
-            }
-            else if(test[j] == 'd') {
+            } else if (test[j] == 'd') {
                 value--;
             } else if (test[j] == 's') {
-                value = (int) Math.sqrt(value);
+                value = Math.pow(value, 2);
             } else if (test[j] == 'o') {
-                secondpoint[secondpoint.length-1] = value;
+                System.out.println((int) value);
+                secondpoint[secondpoint.length - 1] = (int) value;
             }
+
+            for (int i = 0; i < secondpoint.length - 1; i++) {
+              endvalue[i] = (int) value;
+                //System.out.println(Arrays.toString(endvalue));
+            }
+
         }
-
         return secondpoint;
-
     }
-
 }
